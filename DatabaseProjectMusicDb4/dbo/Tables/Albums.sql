b4af74cde783debec33afd50f4b75500314aa823
Tim Roberts <timrobertsusa@pluralpoint.com>
2022-10-30 16:22:30 +0000

    create Tables all MusicDb

--- DatabaseProjectMusicDb4/dbo/Tables/Albums.sql
+++ DatabaseProjectMusicDb4/dbo/Tables/Albums.sql
@@ -1,10 +1,10 @@
 
--- drop table [dbo].[Albums]
+drop table [dbo].[Albums]
 CREATE TABLE [dbo].[Albums] (
-    -- a collection of songs with creators
+    -- a collection of Tracks with creators
     [AlbumId]    INT            IDENTITY (1001, 1) NOT NULL,
     [AlbumTitle] varchar (100) NOT NULL,
-    [PublisherId] int not null,
+   -- [PublisherId] int not null,
     CONSTRAINT [PK_Albums] PRIMARY KEY CLUSTERED ([AlbumId] ASC)
 );
 
